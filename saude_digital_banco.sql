7d0c4e556ffaa4d02066e7200ff97d1cf8864a4a
João Pedro <contato.joaoespindola@gmail.com>
2024-06-23 22:18:29 -0300

    Corrigindo

--- saude_digital_banco.sql
+++ saude_digital_banco.sql
@@ -19,6 +19,24 @@ CREATE TABLE IF NOT EXISTS corretores (
     numero_registro VARCHAR(20) NOT NULL UNIQUE
 );
 
+CREATE TABLE IF NOT EXISTS planos_saude (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_plano VARCHAR(100) NOT NULL,
+    estado VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS corretores_planos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    corretor_id INT NOT NULL,
+    plano_id INT NOT NULL,
+    FOREIGN KEY (corretor_id) REFERENCES corretores(id),
+    FOREIGN KEY (plano_id) REFERENCES planos_saude(id),
+    UNIQUE (corretor_id, plano_id)
+);
+
+INSERT INTO planos_saude (nome_plano, estado) VALUES 
+('Unimed', 'Rio de Janeiro');
+
 INSERT INTO clientes (nome, email, senha, cpf) VALUES
 ('Samuel', 'samuel@example.com', '123', '111.111.111-11'),
 ('Pipico', 'pipico@example.com', '123', '222.222.222-22');
